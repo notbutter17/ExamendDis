0a93fbd7cfac627fb50f48767e9b220793ec0ba5
markmercado1 <markemerson1245@gmail.com>
2025-09-29 14:15:51 -0500

    cambios

--- jgt-ms-config-server/src/main/java/com/cmm/jgtmsconfigserver/JgtMsConfigServerApplication.java
+++ jgt-ms-config-server/src/main/java/com/cmm/jgtmsconfigserver/JgtMsConfigServerApplication.java
@@ -2,6 +2,9 @@ package com.cmm.jgtmsconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
 
 @SpringBootApplication
 public class JgtMsConfigServerApplication {

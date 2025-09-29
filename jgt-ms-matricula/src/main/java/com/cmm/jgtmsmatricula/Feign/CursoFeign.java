a2f46c62628734f72e8c4329dad19950d8f576bd
markmercado1 <markemerson1245@gmail.com>
2025-09-29 14:28:55 -0500

    cambios

--- jgt-ms-matricula/src/main/java/com/cmm/jgtmsmatricula/Feign/CursoFeign.java
+++ jgt-ms-matricula/src/main/java/com/cmm/jgtmsmatricula/Feign/CursoFeign.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "cmm-ms-curso-service", path = "/cursos")
+@FeignClient(name = "jgt-ms-curso-service", path = "/cursos")
 public interface CursoFeign {
 
     @GetMapping("/{id}")
